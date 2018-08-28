82e7d14ba35facc2b5e242adbe8e6f39a67e3010
Mark-Weston <markweston@cock.li>
2018-08-29 02:31:33 +0300

    Spawn window

--- src/main.c
+++ src/main.c
@@ -1,14 +1,65 @@
+#include <stdbool.h>
 #include <stdio.h>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 #define CFG_PATH "assets/cfg"
+#define WIN_TITLE "Sassy Square Sally"
+
+static GLFWwindow*mkWin(int w,int h,const char *t,bool f,bool vsync,int aa);
 
 int main(void)
 {
-    int windowedMode, windowWidth, windowHeight, vsync, aa;
+    int windowed, winW, winH, vsync, aa;
     FILE *f = fopen(CFG_PATH, "r");
-    fscanf(f, "%d%d%d", &windowedMode, &windowWidth, &windowHeight);
+    fscanf(f, "%d%d%d", &windowed, &winW, &winH);
     fscanf(f, "%d%d", &vsync, &aa);
     fclose(f);
-    printf("%d %d %d\n", windowedMode, windowWidth, windowHeight);
-    printf("%d %d\n", vsync, aa);
+
+    glfwInit();
+    GLFWwindow *win = mkWin(winW, winH, WIN_TITLE, !windowed, vsync, aa);
+    while (!glfwWindowShouldClose(win)) {
+        glfwWaitEvents();
+    }
+    glfwTerminate();
 }
+
+static GLFWwindow*mkWin(int w,int h,const char *t,bool f,bool vsync,int aa) {
+    GLFWwindow *win;
+    int width, height;
+    GLFWmonitor *monitor;
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_SAMPLES, aa);
+
+    if (f) {
+        monitor = glfwGetPrimaryMonitor();
+        if (!monitor) {
+            return NULL;
+        }
+        const GLFWvidmode *vidmode = glfwGetVideoMode(monitor);
+        if (!vidmode) {
+            return NULL;
+        }
+        width = vidmode->width;
+        height = vidmode->height;
+        glfwWindowHint(GLFW_RED_BITS, vidmode->redBits);
+        glfwWindowHint(GLFW_GREEN_BITS, vidmode->greenBits);
+        glfwWindowHint(GLFW_BLUE_BITS, vidmode->blueBits);
+        glfwWindowHint(GLFW_REFRESH_RATE, vidmode->refreshRate);
+    } else {
+        width = w;
+        height = h;
+        monitor = NULL;
+    }
+
+    win = glfwCreateWindow(width, height, t, monitor, NULL);
+    glfwMakeContextCurrent(win);
+    glfwSwapInterval(vsync ? 1 : 0);
+
+    return win;
+}
+
