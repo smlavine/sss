dfa344add9f0f802d3cbee0060622de138adef2f
Mark-Weston <markweston@cock.li>
2018-08-29 01:47:47 +0300

    Scan configuration
    Initial window mode, width, height
    Vsync on/off, MSAA sample count

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
-int main(void) {}
+#include <stdio.h>
+
+#define CFG_PATH "assets/cfg"
+
+int main(void)
+{
+    int windowedMode, windowWidth, windowHeight, vsync, aa;
+    FILE *f = fopen(CFG_PATH, "r");
+    fscanf(f, "%d%d%d", &windowedMode, &windowWidth, &windowHeight);
+    fscanf(f, "%d%d", &vsync, &aa);
+    fclose(f);
+    printf("%d %d %d\n", windowedMode, windowWidth, windowHeight);
+    printf("%d %d\n", vsync, aa);
+}
